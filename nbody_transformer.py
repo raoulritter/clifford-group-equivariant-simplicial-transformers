f0b61a5bee3ae6f6cd4a5a04e39cee67a997812a
raoulritter <raoulflorisritter@gmail.com>
2024-05-15 16:05:35 +0200

    CORRECT_TRANSFORMER IMPLEMENTATION

--- nbody_transformer.py
+++ nbody_transformer.py
@@ -47,13 +47,16 @@ def make_edge_attr(node_features, edges, batch_size):
         node_i_features = node_features[node1]  # [#features(charge, loc, vel), dim]
         node_j_features = node_features[node2]  # [#features(charge, loc, vel), dim]
         difference = node_i_features - node_j_features
+        # mean_vector = torch.stack((node_i_features, node_j_features))
+        # mean =  torch.mean(mean_vector, dim=0)
 
         # geom product between node features
-        geom_product = clifford_algebra.geometric_product(node_i_features, node_j_features)
+        # geom_product = clifford_algebra.geometric_product(node_i_features, node_j_features)
 
-        # now just add the 2 to get the "representation of the edge"
-        # Stack all
-        edge_representation = torch.cat((difference, geom_product), dim=0)
+        # now stack all the 2 to get the "representation of the edge"
+        # edge_representation = torch.cat((difference, geom_product), dim=0)
+        # edge_representation = torch.cat((mean, geom_product), dim=0)
+        edge_representation = difference
         edge_attributes.append(edge_representation)
 
     edge_attributes = torch.stack(edge_attributes)
@@ -64,10 +67,10 @@ def embed_nbody_graphs(batch):
 
     batch_size, n_nodes, _ = loc.size()
 
-    # put the mean of the pointcloud on the origin (WHY?)
+    # put the mean of the pointcloud on the origin -> translation invariant
     loc_mean = loc - loc.mean(dim=1, keepdim=True)     # [batch, nodes, dim]
 
-    # ALL THESE do [batch, nodes, dim] ->>> [batch * nodes, dim]
+    # All times do [batch, nodes, dim] -> [batch * nodes, dim]
     loc_mean = loc_mean.float().view(-1, *loc_mean.shape[2:])
     loc = loc.float().view(-1, *loc.shape[2:])
     vel = vel.float().view(-1, *vel.shape[2:])
@@ -81,19 +84,26 @@ def embed_nbody_graphs(batch):
 
     xv = torch.stack([loc_mean, vel], dim=1) # now of the shape [batch * nodes, 2 (because its loc mean as well as vel), dim]
     covariants = clifford_algebra.embed(xv, (1, 2, 3))
-    pos_vel_in_clifford = covariants
 
+    # Create a vector with [n1, n2, n3, e1, e2, e3, e4]
     nodes_in_clifford = torch.cat([invariants[:, None], covariants], dim=1) # [batch * nodes, #features(charge, loc, vel), dim]
+    zero_padding_nodes = torch.zeros(5*batch_size, 4, 8)
+    full_node_embedding = torch.cat((nodes_in_clifford, zero_padding_nodes), dim=1)
 
     batch_index = torch.arange(batch_size, device=loc_mean.device) # torch.arange(batch_size) generates a tensor from 0 to batch_size - 1, creating a sequence that represents each graph in the batch. If batch_size is 3, this tensor will be [0, 1, 2]
     edges = edges + n_nodes * batch_index[:, None, None] # creates separate edge number for every graph. so if edge for graph 1 is between 3 and 4, graph 2 will be between 8 and 9 (if n_nodes = 5)
     edges = tuple(edges.transpose(0, 1).flatten(1)) # where the first element of the tuple contains all start nodes and the second contains all end nodes for edges across the entire batch. ([edges*batch], [edges*batch])
 
-    extra_edge_attr_clifford = make_edge_attr(nodes_in_clifford, edges, batch_size) # [batch*edges, #numfeatures, difference + geomprod, dim]
-
+    # Create a vector with [n1, n2, n3, e1, e2, e3, e4]
     orig_edge_attr_clifford = clifford_algebra.embed(edge_attr[..., None], (0,)) # now [batch * edges, 1, dim]
+    extra_edge_attr_clifford = make_edge_attr(nodes_in_clifford, edges, batch_size) # [batch*edges, #numfeatures, difference + geomprod, dim]
+    edge_attr_all = torch.cat((orig_edge_attr_clifford, extra_edge_attr_clifford), dim=1)
+    zero_padding_edges = torch.zeros(20*batch_size, 3, 8)
+    full_edge_embedding = torch.cat((zero_padding_edges, edge_attr_all), dim=1)
 
-    return nodes_in_clifford, extra_edge_attr_clifford, orig_edge_attr_clifford, loc_end
+    loc_end_clifford = clifford_algebra.embed(loc_end, (1, 2, 3))
+
+    return full_node_embedding, full_edge_embedding, loc_end_clifford
 
 def generate_mock_batch(batch_size):
     """
@@ -132,98 +142,125 @@ class PositionalEncoding(nn.Module):
 
         # Adding a binary feature
         # Nodes get a [1, 0] and edges get a [0, 1]
-        # node_marker = torch.zeros(5*batch_size, 1, 8)
-        edge_marker = torch.ones(20*batch_size, 1, 8)
+        node_marker = torch.zeros(5*batch_size, 1, 8)
+
+        # Create a tensor of zeros with shape [20*batch_size, 1, 8] with a 1 at the beginning (onehot)
+        edge_marker = torch.zeros(20 * batch_size, 1, 8)
+        edge_marker[:, :, 0] = 1
 
         # Concatenate this new feature
-        # self.pe = torch.cat((node_marker, edge_marker), dim=0)
-        self.pe = edge_marker
+        self.pe = torch.cat((node_marker, edge_marker), dim=0)
 
     def forward(self, x):
         print(x.shape, self.pe.shape)
         return torch.cat((x, self.pe), dim=1)
 
-def scaled_dot_product_attention(query, key, value, mask=None):
-    d_k = query.size(-1)
-    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
-    if mask is not None:
-        scores = scores.masked_fill(mask == 0, -1e9)
-    attn = torch.softmax(scores, dim=-1)
-    output = torch.matmul(attn, value)
-    return output, attn
+class SelfAttentionClifford(nn.Module):
+    def __init__(self, num_feat, num_nodes, num_edges, algebra):
+        super(SelfAttentionClifford, self).__init__()
+        self.num_feat = num_feat
+        self.num_nodes = num_nodes
+        self.num_edges = num_edges
+        self.algebra = algebra
+        self.q_linear = MVLinear(algebra, num_feat, 1, subspaces=True)
+        self.k_linear = MVLinear(algebra, num_feat, 1, subspaces=True)
+        self.v_linear = MVLinear(algebra, num_feat, 1, subspaces=True)
+        self.output_embedding = MVLinear(algebra, 1*2, num_feat, subspaces=True)
+
+    def forward(self, feature_matrix):
+        bs = feature_matrix.size(0)//25
+
+        # Compute query, key, and value matrices
+        q = self.q_linear(feature_matrix)
+        k = self.k_linear(feature_matrix)
+        v = self.v_linear(feature_matrix)
+
+        # Compute dot product for attention
+        q1_reshape = q.view(25*bs, -1)
+        k1_reshape = k.view(25*bs, -1)
+
+        attn = torch.mm(q1_reshape, k1_reshape.T)  # (bs*(num_nodes + num_edges), num_feat, 8)
+        # Normalize the attention weights with d normally
+        attn = attn / math.sqrt(k.size(-1))
+        attn = F.softmax(attn, dim=-1)
+
+        v_reshaped = v.squeeze(1)
+        attention_feature_matrix = torch.matmul(attn, v_reshaped)
+        attention_feature_matrix = attention_feature_matrix.unsqueeze(1)
+
+        # Apply geometric product but might not be necessary let's check with Cong.
+        gp_feature_matrix = self.geometric_product(attention_feature_matrix, attention_feature_matrix)
+
+        concat_feature_matrix = torch.cat((attention_feature_matrix, gp_feature_matrix), dim=1)
+        embed_output = self.output_embedding(concat_feature_matrix)
+
+        return embed_output
+
+    def geometric_product(self, a, b):
+        return self.algebra.geometric_product(a, b)
 
 class GAST_block(nn.Module):
     def __init__(self, d_model, num_heads, clifford_algebra, channels):
         super(GAST_block, self).__init__()
         self.mvlayernorm = MVLayerNorm(clifford_algebra, channels)
-        self.self_attn = nn.MultiheadAttention(d_model, num_heads)
-        self.mvlayernorm2 = MVLayerNorm(clifford_algebra, channels)
-        self.cemlp = CEMLP(clifford_algebra, d_model, d_model, d_model)
+        self.self_attn = SelfAttentionClifford(7, 5, 20, clifford_algebra)
+        # self.mvlayernorm2 = MVLayerNorm(clifford_algebra, channels)
 
     def forward(self, src, src_mask=None):
         src = self.mvlayernorm(src)
-        q, k = src, src
-        v = src
-        attn_output, _ = self.self_attn(q, k, v, attn_mask=src_mask)
-        src = self.mvlayernorm2(attn_output)
-        src = self.cemlp(src)
+        src = self.self_attn(src)
+        #add layernorm here.
         return src
-
-
-class GAST(nn.Module):
-    def __init__(self, num_layers, d_model, num_heads,  clifford_algebra, channels):
-        super(GAST, self).__init__()
-        self.layers = nn.ModuleList([GAST_block(d_model, num_heads) for _ in range(num_layers)])
-        self.norm = nn.LayerNorm(d_model)
-
-    def forward(self, src, src_mask=None):
-        for layer in self.layers:
-            src = layer(src, src_mask)
-        return self.norm(src)
 
 class GAST(nn.Module):
     def __init__(self, num_layers, d_model, num_heads, clifford_algebra, channels):
         super(GAST, self).__init__()
-        self.layers = nn.ModuleList([GAST_block(d_model, num_heads, clifford_algebra, channels) for _ in range(num_layers)])
-        self.norm = nn.LayerNorm(d_model)
-
+        # self.activation = MVSiLU(clifford_algebra)
+        self.layers = nn.ModuleList(
+            [GAST_block(d_model, num_heads, clifford_algebra, channels) for _ in range(num_layers)])
+        # self.norm = nn.LayerNorm(d_model)
     def forward(self, src, src_mask=None):
         for layer in self.layers:
             src = layer(src, src_mask)
-        return self.norm(src)
+        return src
 
 class NBODY_Transformer(nn.Module):
-    def __init__(self, input_dim, d_model, num_heads, num_layers, mv_in_features, mv_out_features, clifford_algebra, channels, batch_size):
+    def __init__(self, input_dim, d_model, num_heads, num_layers, batch_size, embed_in_features, embed_out_features, clifford_algebra, channels):
         super(NBODY_Transformer, self).__init__()
         self.positional_encoding = PositionalEncoding(d_model, batch_size)
         self.GAST = GAST(num_layers, d_model, num_heads, clifford_algebra, channels)
-        self.embedding = MVLinear(clifford_algebra, mv_out_features, mv_out_features, subspaces=False)
-        self.fc = MVLinear(clifford_algebra, d_model, input_dim, subspaces=True)
-        self.edge_to_node_shape = MVLinear(clifford_algebra, mv_in_features, mv_out_features, subspaces=True)
+        self.embedding = MVLinear(clifford_algebra, embed_in_features, embed_out_features, subspaces=False)
 
     def forward(self, nodes_in_clifford, edges_in_clifford, src_mask, batch_size):
-        edges_in_clifford = self.positional_encoding(edges_in_clifford)
-        fully_embedded_edges = self.edge_to_node_shape(edges_in_clifford)
-        src = torch.cat((nodes_in_clifford, fully_embedded_edges), dim=0)
-        src = self.embedding(src)
+
+        # POSITIONAL ENCODING left out for now
+        # edges_in_clifford = self.positional_encoding(edges_in_clifford)
+        src = torch.cat((nodes_in_clifford, edges_in_clifford), dim=0)
+
+        # src = self.embedding(src)
         enc_output = self.GAST(src, src_mask)
-        output = self.fc(enc_output)
-        return output
+        # output = self.fc(enc_output)
+        output = enc_output
+
+        # return only nodes and only the "pos" feature vector of the nodes
+        return output[:(5*batch_size), 1, :]
 
 # Hyperparameters
-input_dim = 8
+input_dim = 7  # feature_dim
 d_model = 512
 num_heads = 8
 num_layers = 6
-mv_in_features = 8
-mv_out_features = 3
-channels = 8
-batch_size = 1  # Define batch_size here
+embed_in_features = 3
+embed_out_features = 3
+batch_size = 1
+channels = 7 #???? I have in my notesd 4 for some reason
+
 
 # Create the model
-model = NBODY_Transformer(input_dim, d_model, num_heads, num_layers, mv_in_features, mv_out_features, clifford_algebra, channels, batch_size)
+model = NBODY_Transformer(input_dim, d_model, num_heads, num_layers, batch_size, embed_in_features, embed_out_features, clifford_algebra, channels)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
+
 
 batch = generate_mock_batch(batch_size)
 src_mask = None
@@ -232,12 +269,12 @@ src_mask = None
 model.train()
 for epoch in range(10):
     optimizer.zero_grad()
+    batch = batch
     components = embed_nbody_graphs(batch)
-    nodes_in_clifford, extra_edge_attr_clifford, orig_edge_attr_clifford, loc_end = components
-    edges_in_clifford = torch.cat((extra_edge_attr_clifford, orig_edge_attr_clifford), dim=1)
-    tgt = loc_end
+    nodes, edges, tgt = components
 
-    output = model(nodes_in_clifford, edges_in_clifford, src_mask, batch_size)
+    output = model(nodes, edges, src_mask, batch_size)
+
     loss = criterion(output, tgt)
     loss.backward()
     optimizer.step()
